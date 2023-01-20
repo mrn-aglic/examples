7470daece18b744e490a7f2220081eb780a82f3f
Marin Aglic <mrn.aglic@gmail.com>
2023-01-20 22:37:24 +0100

    Added shell script for viewing logs inside tasks of mapped task group instances
    At the time of committing this the Airflow webserver UI does not display logs for tasks that are located inside mapped task groups.
    This simple script with the assumptions about the DAG id, task id and map index can print the log for the latest attempt.

--- airflow-new-features/view_latest_scheduler_log.sh
+++ airflow-new-features/view_latest_scheduler_log.sh
@@ -1,10 +1,34 @@
 #!/bins/sh
 
-for file in $(find . -type f -iname ".env.backup");
-do
-  target=${file%.backup}
 
-  echo "Copying $file to $target"
+DAG_ID=task_group_mapping_example
+TASK_ID=task_id=batch_processing.transfer_to_s3
+MAP_INDEX=0
 
-  cp $file $target
-done;
+SCHEDULER_CONTAINER_NAME=airflow-feat-scheduler
+
+echo "$SCHEDULER_CONTAINER_NAME"
+
+DIR="logs/dag_id=$DAG_ID/"
+
+LAST_RUN_DIR=$(docker exec "$SCHEDULER_CONTAINER_NAME" ls -Artls $DIR | tail -1)
+
+LAST_RUN_DIR=$(echo "$LAST_RUN_DIR" | rev | cut -d" " -f1 | rev)
+
+CURR_DIR="$DIR/$LAST_RUN_DIR"
+
+TARGET_DIR="$CURR_DIR/$TASK_ID"
+
+if [ $MAP_INDEX -ge 0 ]
+then
+  TARGET_DIR="$TARGET_DIR/map_index=$MAP_INDEX"
+fi
+
+docker exec "$SCHEDULER_CONTAINER_NAME" ls -Artls "$TARGET_DIR"
+
+LAST_RUN_ATTEMPT=$(docker exec "$SCHEDULER_CONTAINER_NAME" ls -Artls "$TARGET_DIR" | tail -1)
+LAST_RUN_ATTEMPT=$(echo "$LAST_RUN_ATTEMPT" | rev | cut -d" " -f1 | rev)
+
+TARGET_FILE="$TARGET_DIR/$LAST_RUN_ATTEMPT"
+
+docker exec "$SCHEDULER_CONTAINER_NAME" cat "$TARGET_FILE"
