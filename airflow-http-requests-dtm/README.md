04dd0edaa3c377b84eb939d57800c3709ae83035
Marin Aglic <mrn.aglic@gmail.com>
2023-03-25 23:58:27 +0100

    Collect weather data dag added

--- airflow-http-requests-dtm/README.md
+++ airflow-http-requests-dtm/README.md
@@ -4,3 +4,6 @@
 This example shows how to use dynamic task mapping to make
 multiple HTTP requests for collecting data in Airflow. The
 accompanying Medium article is published: 
+
+1. collect data -> (dtm) -> httpasynchook
+2. collect data -> deferrable operator
---
+++ airflow-http-requests-dtm/plugins/httpasync/httptrigger.py
@@ -0,0 +1,67 @@
+import asyncio
+from typing import Any, AsyncIterator, Dict, Optional, Tuple
+
+from airflow import AirflowException
+from airflow.providers.http.hooks.http import HttpAsyncHook
+from airflow.triggers.base import BaseTrigger, TriggerEvent
+
+
+class HttpTrigger(BaseTrigger):
+    # pylint: disable=too-many-arguments
+    def __init__(
+        self,
+        endpoint: str,
+        http_conn_id: str,
+        method: str = "GET",
+        data: Optional[dict] = None,
+        headers: Optional[dict] = None,
+        retry_limit: int = 3,
+        retry_delay: float = 1.0,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.endpoint = endpoint
+        self.http_conn_id = http_conn_id
+        self.method = method
+        self.data = data or {}
+        self.headers = headers or {}
+        self.retry_limit = retry_limit
+        self.retry_delay = retry_delay
+
+    async def run(self) -> AsyncIterator["TriggerEvent"]:
+        http_hook = self._get_async_hook()
+
+        retry_num = 1
+
+        while retry_num <= self.retry_limit:
+            try:
+                response = await http_hook.run(
+                    endpoint=self.endpoint, data=self.data, headers=self.headers
+                )
+
+                data = await response.json()
+                yield TriggerEvent({"data-length": len(data), "data": data})
+            except AirflowException as e:
+                await asyncio.sleep(self.retry_delay)
+
+                retry_num = retry_num + 1
+
+                if retry_num > self.retry_limit:
+                    raise e
+
+    def serialize(self) -> Tuple[str, Dict[str, Any]]:
+        return (
+            "dags.httpasync.httptrigger.HttpTrigger",
+            {
+                "endpoint": self.endpoint,
+                "http_conn_id": self.http_conn_id,
+                "method": self.method,
+                "headers": self.headers,
+                "retry_limit": self.retry_limit,
+                "retry_delay": self.retry_delay,
+                "data": self.data,
+            },
+        )
+
+    def _get_async_hook(self):
+        return HttpAsyncHook(http_conn_id=self.http_conn_id, method=self.method)
